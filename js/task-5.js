87649309a8dc873a2db1905258a09a898cd06078
Andrii Chornyi <andchorniy@gmail.com>
2020-11-28 14:44:48 +0100

    fix task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,32 +1,31 @@
 const counrtyDelivery = prompt("Укажите страну досавки");
-let delivery;
+let delivery = 0;
 if (counrtyDelivery === null) {
     console.log('Отменено пользователем!');
 } else {
     switch (counrtyDelivery.toLowerCase()) {
     case 'китай':
         delivery = 100;
-        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`)
         break;
     case 'чили':
         delivery = 250;
-        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`)
         break;
     case 'австралия':
         delivery = 170;
-        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`)    
         break;
     case 'индия':
         delivery = 80;
-        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`)    
         break;
     case 'ямайка':
         delivery = 120;
-        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`)    
         break;
     default:
         alert('В вашей стране доставка не доступна');
         
     }
+    if (delivery !== 0) {
+        console.log(`Доставка в ${counrtyDelivery} будет стоить ${delivery} кредитов`);
+    }
+    
 }
 
